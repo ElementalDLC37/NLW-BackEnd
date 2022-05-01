21234a3aa81ab3dbad06f27223c9ca7ca6136801
Lucas <lucasantony815@gmail.com>
2022-04-30 22:07:04 -0300

    Genesis

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateMessageController } from "./controller/CreateMessageController";
+import { Get3LastMessagesController } from "./controller/GetLast3MessagesController";
+import { ProfileUserController } from "./controller/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLast3MessagesService } from "./services/GetLast3MessagesService";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
+export { router }
